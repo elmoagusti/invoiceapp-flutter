c12ed52e1a9625767d7bfcdf5af0dba1861c6dfa
elmoagusti <elmoagusti.ea@gmail.com>
2022-08-18 11:15:14 +0700

    category

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -2,13 +2,6 @@ class Category {
   int? id;
   String? name;
 
-  // categoryMap() {
-  //   var mapping = Map<String, dynamic>();
-  //   mapping['id'] = id;
-  //   mapping['name'] = name;
-  //   return mapping;
-  // }
-
   Map<String, dynamic> categoryMap() => {
         "id": id,
         "name": name,
